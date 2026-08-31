78af1990d02340ead2276b4c720cb8d0b6ce52db
shwetam119 <goelshwetam@gmail.com>
2026-08-31 20:19:41 +0530

    Time: 4 ms (15.43%), Space: 14.8 MB (10.49%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int,int> mpp;
+        for (int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+
+        }
+
+        for(auto it:mpp){
+            if(it.second==1){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
